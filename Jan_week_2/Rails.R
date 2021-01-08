64c569a189c08444810f7eac0e4c78725745ab8a
Raul-Nicolas <rmateosncc@gmail.com>
2021-01-08 18:14:04 +0900

    attempts to plot

--- Jan_week_2/Rails.R
+++ Jan_week_2/Rails.R
@@ -1,19 +1,14 @@
 library(ggplot2)
 library(plotly)
 library(gganimate)
-
+library("RColorBrewer")
 #load data
 tuesdata <- tidytuesdayR::tt_load('2021-01-05')
 td = tuesdata$transit_cost
 td = td[!is.na(td$country),]
 td = td %>% 
-  mutate( n_stations = ifelse(stations %in% 1:5, "Between 1 and 5",
-                              ifelse(stations %in% 6:10, "Between 21 and 40",
-                                     ifelse(stations %in% 41:60, "Between 41 and 60",
-                                            ifelse(stations %in% 61:80, "Between 61 and 80",
-                                                   ifelse(stations %in% 81:100, "Between 81 and 100",
-                                                           "More than 100"))))))
-                            
+  mutate(speed = length/(as.numeric(end_year) - as.numeric(start_year)))
+
 
 
 has10 = td[td$country %in% names(which(table(td$country) > 10)),]
@@ -27,6 +22,24 @@ ggplot(data = has10 , aes(x = country, y = stations)) +
 ggplot(data = has10 , aes(x = year, y = as.numeric(real_cost), col = country)) + 
   geom_point() 
 
-+ 
-  scale_color_viridis() #+ ylim(c(0,50000))
-sdasasd
+ #+ ylim(c(0,50000))
+
+
+
+
+ggplot(data = td , aes(x = as.numeric(cost_km_millions), y = speed, col = country)) + 
+  geom_point() 
+
+ggplot(data = has10 , aes(x = as.numeric(cost_km_millions), y = speed, col = country)) + 
+  geom_point() + 
+  scale_color_viridis(discrete = T) + 
+  theme_dark()
+
+
+
+ggplot(data = has10 , aes(x = as.numeric(length), y = speed , col = country)) + 
+  geom_point() + 
+  theme_dark()+ 
+    scale_color_manual(values = brewer.pal(n = 10, name = "Paired"))
+
+
